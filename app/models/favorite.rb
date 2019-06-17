832ba0e996d739f2568d855e07423f49462128ee
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-06-17 10:44:32 -0700

    Favorites controller and database created and tested

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -16,14 +16,11 @@ class Favorite
 
         return results.map do |result|
             {
-              "entry_id" => result["entry_id"].to_i,
-              "date" => result["date"],
-              "brand" => result["brand"],
-              "origin" => result["origin"],
-              "location" => result["location"],
-              "rating" => result["rating"].to_i,
-              "favorite" => result["favorite"],
-              "flavors" => result["flavors"]
+              "id" => result["id"].to_i,
+              "name" => result["name"],
+              "shortname" => result["shortname"],
+              "address" => result["address"],
+              "city" => result["city"]
             }
         end
     end
@@ -32,41 +29,35 @@ class Favorite
           <<-SQL
               SELECT
                 * FROM favorites
-              WHERE entry_id=#{id};
+              WHERE id=#{id};
           SQL
         )
         return {
-            "entry_id" => results.first["entry_id"].to_i,
-            "date" => results.first["date"],
-            "brand" => results.first["brand"],
-            "origin" => results.first["origin"],
-            "location" => results.first["location"],
-            "rating" => results.first["rating"].to_i,
-            "favorite" => results.first["favorite"],
-            "flavors" => results.first["flavors"]
+            "id" => results.first["id"].to_i,
+            "name" => results.first["name"],
+            "shortname" => results.first["shortname"],
+            "address" => results.first["address"],
+            "city" => results.first["city"]
         }
     end
     def self.create(opts)
         results = DB.exec(
             <<-SQL
-                INSERT INTO favorites (date, brand, origin, location, rating, favorite, flavors)
-                VALUES ( '#{opts["date"]}', '#{opts["brand"]}', '#{opts["origin"]}', '#{opts["location"]}', #{opts["rating"]}, #{opts["favorite"]}, '#{opts["flavors"]}' )
-                RETURNING entry_id, date, brand, origin, location, rating, favorite, flavors;
+                INSERT INTO favorites (name, shortname, address, city)
+                VALUES ( '#{opts["name"]}', '#{opts["shortname"]}', '#{opts["address"]}', '#{opts["city"]}' )
+                RETURNING id, name, shortname, address, city;
             SQL
         )
         return {
-            "entry_id" => results.first["entry_id"].to_i,
-            "date" => results.first["date"],
-            "brand" => results.first["brand"],
-            "origin" => results.first["origin"],
-            "location" => results.first["location"],
-            "rating" => results.first["rating"].to_i,
-            "favorite" => results.first["favorite"],
-            "flavors" => results.first["flavors"]
+            "id" => results.first["id"].to_i,
+            "name" => results.first["name"],
+            "shortname" => results.first["shortname"],
+            "address" => results.first["address"],
+            "city" => results.first["city"]
         }
     end
     def self.delete(id)
-        results = DB.exec("DELETE FROM favorites WHERE entry_id=#{id};")
+        results = DB.exec("DELETE FROM favorites WHERE id=#{id};")
         return { "deleted" => true }
     end
 
@@ -74,22 +65,17 @@ class Favorite
         results = DB.exec(
             <<-SQL
                 UPDATE favorites
-                SET date='#{opts["date"]}', brand='#{opts["brand"]}', origin='#{opts["origin"]}', location='#{opts["location"]}', rating=#{opts["rating"]},
-                favorite=#{opts["favorite"]},
-                flavors='#{opts["flavors"]}'
-                WHERE entry_id=#{id}
-                RETURNING entry_id, date, brand, origin, location, rating, favorite, flavors;
+                SET name='#{opts["name"]}', shortname='#{opts["shortname"]}', address='#{opts["address"]}', city='#{opts["city"]}'
+                WHERE id=#{id}
+                RETURNING id, name, shortname, address, city;
             SQL
         )
         return {
-            "entry_id" => results.first["entry_id"].to_i,
-            "date" => results.first["date"],
-            "brand" => results.first["brand"],
-            "origin" => results.first["origin"],
-            "location" => results.first["location"],
-            "rating" => results.first["rating"].to_i,
-            "favorite" => results.first["favorite"],
-            "flavors" => results.first["flavors"]
+            "id" => results.first["id"].to_i,
+            "name" => results.first["name"],
+            "shortname" => results.first["shortname"],
+            "address" => results.first["address"],
+            "city" => results.first["city"]
         }
     end
 
